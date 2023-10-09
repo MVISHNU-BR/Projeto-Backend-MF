0479a39e70a5c34d2010b15cebc9cd52021c330f
Mahavishnu Jneesh <mahavishnu.afonso@sga.pucminas.br>
2023-10-09 16:27:28 -0300

    adicionado ao crud o controller editar e details

--- Projeto-Backend-MF/Controllers/VeiculosController.cs
+++ Projeto-Backend-MF/Controllers/VeiculosController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 using Projeto_Backend_MF.Models;
 
 namespace Projeto_Backend_MF.Controllers
@@ -18,5 +19,68 @@ namespace Projeto_Backend_MF.Controllers
             
             return View(dados);
         }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Veiculo veiculo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Veiculos.Add(veiculo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(veiculo);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+                var dados = await _context.Veiculos.FindAsync(id);
+               
+                if (dados == null)
+                    return NotFound();
+            
+                
+            return View(dados); 
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Veiculo veiculo)
+        {
+            if (id != veiculo.Id)
+                return NotFound();
+
+            if(ModelState.IsValid)
+            {
+                _context.Veiculos.Update(veiculo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Veiculos.FindAsync(id);
+            
+            if(dados == null)
+                return NotFound();
+
+            return View(dados);
+        }
+        
     }
 }
